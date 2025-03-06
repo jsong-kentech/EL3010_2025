b59da5681a80f4882d8f54e30bd387f387bda447
jsong-kentech <jsong@kentech.ac.kr>
2025-03-06 11:21:14 +0900

    1
    1

---
+++ EL3010_2_random_walk_1d.m
@@ -0,0 +1,71 @@
+%% Random walk 
+% 2024.02.25
+% Ju Song
+% KENTECH
+
+% Contents
+% (1) Config 
+% (2) 2D Random walk (to visualize)
+% (3) 1D Random walk (to analyze)
+
+clear; clc; close all
+%% Config
+dx = 1;
+dt = 1;
+
+N = 10^3; % number of random walkers
+t_end = 10000; % time period to simulate
+t_vec = 0:dt:t_end;
+M = length(t_vec);
+
+
+%% 1D Random walk
+
+% initialize
+x_mat = zeros(N,M);
+
+% random walks
+% time step loop
+for m = 1:M
+    
+    % generate random walk for N particles
+    dx_now = dx*(2*unidrnd(2,N,1)-3);
+    
+    % calculate position
+    if m ==1
+       x_mat(:,m) = zeros(N,1);
+    else
+        x_mat(:,m) = x_mat(:,m-1) + dx_now;
+    end
+end
+
+
+% analyze: average distance
+figure(5)
+plot(t_vec,x_mat)
+xlabel('time')
+ylabel('x-position')
+
+
+figure(6)
+% histogram
+histogram(x_mat(:,1),'Normalization','pdf'); hold on
+histogram(x_mat(:,2000),'Normalization','pdf')
+histogram(x_mat(:,3000),'Normalization','pdf')
+histogram(x_mat(:,5000),'Normalization','pdf')
+histogram(x_mat(:,10000),'Normalization','pdf')
+
+ylim([0 0.02])
+
+d_mat = sqrt(x_mat.^2);
+d_avg = mean(d_mat,1);
+figure(7) 
+plot(t_vec,d_avg)
+xlabel('time')
+ylabel('avg distance')
+
+figure(8) 
+plot(t_vec.^0.5,d_avg)
+xlabel('time^{0.5}')
+ylabel('avg distance')
+
